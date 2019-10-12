e89de340c40d6499d9924d765f2ecc29e3869cd7
Qastel <34581614+Qastel@users.noreply.github.com>
2019-10-12 21:55:02 +0100

    added timmer page by @Razvan

--- Client_UI.py
+++ Client_UI.py
@@ -2,6 +2,8 @@
 from Client import *
 from tkinter import *
 from tkinter.font import Font
+from time import strftime
+import math
 client_connect_server()
 
 
@@ -10,27 +12,27 @@ def clear_window():
         widget.destroy()
 
 
-def show_back_button():
-    back_button = Button(text="<<", command=login)
+def show_back_button(previous_page):
+    back_button = Button(text="<<", command=previous_page)
     back_button.place(x=10, y=10, width=50, height=25)
     back_button.configure(background="#66A1DC")
 
 
-def map_window():
+def map_page():
     clear_window()
 
-    show_back_button()
+    show_back_button(login_page)
 
     info_label = Label(text="Select your bike station: ")
     info_label.place(x=150, y=50)
     info_label.configure(font=14)
 
-    # marker = PhotoImage(file="location2.png")
-    # label_image = Label(image=marker)
-    # label_image.image = marker  # keep a reference
-    # label_image.place(x=180, y=90, width=128, height=128)
+    marker = PhotoImage(file="./resources/location_icon.png")
+    label_image = Label(image=marker)
+    label_image.image = marker  # keep a reference
+    label_image.place(x=180, y=90, width=128, height=128)
 
-    start_y = 200
+    start_y = 250
 
     locationA_button = Button(text="Location A", command=clear_window)
     locationA_button.place(x=150, y=start_y + 50, width=200, height=25)
@@ -65,7 +67,7 @@ def clientLogin(un, pw):
         register_label.place(x=150, y=220)
         register_label.configure(fg="red")
     elif login_state == "USER_VERIFIED":
-        map_window()
+        map_page()
 
 
 def registerClient(un, pw):
@@ -83,7 +85,7 @@ def registerClient(un, pw):
         register_label.place(x=150, y=220)
         register_label.configure(fg="red")
     elif regis_state == "USER_REGISTERD":
-        map_window()
+        map_page()
 
 
 # def register(mobile, password):
@@ -96,14 +98,14 @@ def registerClient(un, pw):
 #         cursor.execute("""INSERT INTO user(mobile,password) VALUES(?,?)""", (mobile, password))
 #         db.commit()
 #         ## return to map
-#         map_window()
+#         map_page()
 #
 #
-def open_register():
+def register_page():
     clear_window()
-    show_back_button()
+    show_back_button(login_page)
 
-    logo = PhotoImage(file="./resources/bikeshare_icon.png")
+    logo = PhotoImage(file="./resources/bikeshare_icon2.png")
     label_image = Label(image=logo)
     label_image.image = logo
     label_image.place(x=150, y=40, width=200, height=140)
@@ -131,10 +133,10 @@ def open_register():
     register_button.configure(font=3)
 
 
-def login():
+def login_page():
     clear_window()
 
-    logo = PhotoImage(file="./resources/bikeshare_icon.png")
+    logo = PhotoImage(file="./resources/bikeshare_icon2.png")
     label_image = Label(image=logo)
     label_image.image = logo
     label_image.place(x=150, y=40, width=200, height=140)
@@ -166,17 +168,68 @@ def login():
     register_label.place(x=150, y=450)
     register_label.configure(font=("Calibri", 8))
 
-    register_button = Button(text="Register", command=open_register)
+    register_button = Button(text="Register", command=register_page)
     register_button.place(x=300, y=450, width=50, height=20)
 
 
+def return_bike_page(hours, minutes):
+    print(hours, minutes)
+    
+
+
+def timmer(s_initial, time_label):
+    global first_timmer
+    global minutes
+    global hours
+    
+    s = int(strftime("%S"))
+    
+    if s < s_initial:
+        sec = s + 60 - s_initial
+    else:
+        sec = s - s_initial
+
+    if sec == 0 and first_timmer == False:
+        minutes = (minutes + 1) % 60
+        if minutes == 0:
+            hours = (hours + 1) % 60
+    else:
+        first_timmer = False
+        
+    time_label.configure(text = str(hours) + ":" + str(minutes) + ":" + str(sec)  )
+    time_label.after(1000, lambda: timmer(s_initial, time_label) )
+
+def timmer_page():
+    
+    title = Label(text = "Your trip has started", font = ('Helvetica', 18))
+    title.place(x = 140, y = 70)
+
+    time_title = Label(text="H :   M:   S:", font = ('Helvetica', 18) )
+    time_title.place(x = 180, y = 180)
+
+    time_label = Label(text="", font = ('Helvetica', 48), fg='red')
+    time_label.place(x = 170, y = 210)
+
+    return_button = Button(text = "Return Bike", command = lambda: return_bike_page(hours, minutes), font = ('Helvetica', 12))
+    return_button.place(x = 150, y = 400, width = 200, height = 25)
+
+    timmer( int(strftime("%S")), time_label )
+
 ######## main #########
+    
+def main_page():
+    window.title("BikeSharing")
+    window.geometry("500x530")
+    global my_font
+    my_font = Font(size=12)
+    login_page()
+    window.mainloop()
+
 window = Tk()
-window.title("BikeSharing")
-window.geometry("500x530")
+my_font 
+minutes = 0
+hours = 0
+first_timmer = True
+main_page()
 
-my_font = Font(size=12)
 
-login()
-
-window.mainloop()
